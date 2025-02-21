6b326b0ece4350fc4126a4fed3b36602df45043f
BlaiseI <BlaiseWorlds@gmail.com>
2025-02-21 22:09:03 +0100

    imgs change + no match at beginning
    Changed block images size to 60, block size to 67. Added frames outside blocks for clearer grid. At grid generation, block type changes if it would generate a match.

--- Scripts/grid.gd
+++ Scripts/grid.gd
@@ -15,25 +15,44 @@ var blocks = [
 	preload("res://Scenes/block_egopode.tscn")
 ]
 
+var cadrePreload = preload("res://Scenes/cadre.tscn")
+
 var rng: RandomNumberGenerator = RandomNumberGenerator.new()
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	initGrid()
 
-func initGrid() ->void:
+func initGrid() -> void:
 	for i in width:
 		grid.append([])
 		for j in height:
 			grid[i].append(null)
-			
+			var cadre = cadrePreload.instantiate()
+			add_child(cadre)
+			cadre.position = getTileCoordsFromPosition(i,j)
 	for i in width:
 		for j in height:
-			var blockIndex: int = rng.randi_range(0, blocks.size()-1)
-			var block = blocks[blockIndex].instantiate()
+			var isMatch: bool = true
+			var block
+			while(isMatch):
+				var blockIndex: int = rng.randi_range(0, blocks.size()-1)
+				block = blocks[blockIndex].instantiate()
+				if not givesMatch(j,i,block) :
+					isMatch = false
 			add_child(block)
 			block.position = getTileCoordsFromPosition(i,j)
+			grid[i][j] = block
 	pass
+	
+func givesMatch(row: int, column: int, block) -> bool:
+	if row >= 2 :
+		if (block.blockType == grid[column][row-1].blockType and block.blockType == grid[column][row-2].blockType):
+			return true
+	if column >= 2 :
+		if (block.blockType == grid[column-1][row].blockType and block.blockType == grid[column-2][row].blockType):
+			return true	
+	return false
 
 func getTileCoordsFromPosition(row: int, column: int) -> Vector2:
 	var xCoord: int = xStart + (column*offset)
