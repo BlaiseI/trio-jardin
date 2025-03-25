726d02aaf11ea05436483f5b36f00bb1db0a3b21
BlaiseI <BlaiseWorlds@gmail.com>
2025-03-25 17:49:25 +0100

    Added empty tiles possibilities
    Added possibility to have empty spaces, with 2 possibilities to fill in back the tiles under : either blocks from above empty tile cross the empty tile to fill the tile above; or the tiles up until the empty tile fall, and new ones are created under the empty tiles

--- Scripts/grid.gd
+++ Scripts/grid.gd
@@ -11,13 +11,15 @@ var state
 @export var xStart: int
 @export var yStart: int
 @export var offset: int
+@export var emptyTiles : PackedVector2Array
 
 var grid = []
 
 var cadrePreload = preload("res://Scenes/cadre.tscn")
 
 var slideBeginCoords: Vector2
-var slideEndCoords: Vector2 = Vector2(-1, -1)
+var slideBeginPosition: Position
+var slideEndPosition: Position
 var slideOngoing: bool = false
 
 # Called when the node enters the scene tree for the first time.
@@ -34,38 +36,45 @@ func createEmptyGrid() -> void:
 		grid.append([])
 		for j in width:
 			grid[i].append(null)
-			var cadre = cadrePreload.instantiate()
-			add_child(cadre)
-			cadre.position = utils.getTileCoordsFromPosition(Position.new(i,j), self)
+			if !emptyTile(i,j):
+				var cadre: Block = cadrePreload.instantiate()
+				add_child(cadre)
+				cadre.position = utils.getTileCoordsFromPosition(Position.new(i,j), self)
+				cadre.get_node("CenterContainer/Control/Sprite2D").modulate.a = 0.2
 
 func fillGrid() -> void:
 	for i in height:
 		for j in width:
-			var isMatch: bool = true
-			var block: Node = createNonMatchingBlock(i,j)
-			add_child(block)
-			block.position = utils.getTileCoordsFromPosition(Position.new(i,j), self)
-			grid[i][j] = block
+			if !emptyTile(i,j):
+				var isMatch: bool = true
+				var block: Node = createNonMatchingBlock(i,j)
+				add_child(block)
+				block.position = utils.getTileCoordsFromPosition(Position.new(i,j), self)
+				grid[i][j] = block
 
 func createNonMatchingBlock(row: int, column: int) -> Block:
 	var block: Block
 	var isMatch: bool = true
 	while(isMatch):
 		block = Block.createRandomBlock()
-		if not givesMatch(column, row, block) :
+		if not givesMatch(row, column, block) :
 			isMatch = false
 	return block
 
 func givesMatch(row: int, column: int, block) -> bool:
-	if row >= 2 :
-		if (block.blockType == grid[column][row-1].blockType and block.blockType == grid[column][row-2].blockType):
+	if column >= 2 and !emptyTile(row,column-1) and !emptyTile(row,column-2):
+		if (block.blockType == grid[row][column-1].blockType and block.blockType == grid[row][column-2].blockType):
 			return true
-	if column >= 2 :
-		if (block.blockType == grid[column-1][row].blockType and block.blockType == grid[column-2][row].blockType):
-			return true	
+	if row >= 2 and !emptyTile(row-1,column) and !emptyTile(row-2,column):
+		if (block.blockType == grid[row-1][column].blockType and block.blockType == grid[row-2][column].blockType):
+			return true
 	return false
 
-
+func emptyTile(row: int, column:int) -> bool:
+	for coords: Vector2 in emptyTiles:
+		if coords == Vector2(row, column):
+			return true
+	return false
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.		
 func _process(delta: float) -> void:
@@ -79,11 +88,16 @@ func _process(delta: float) -> void:
 func checkMovement() -> void:
 	if Input.is_action_just_pressed("ui_touch"):
 		slideBeginCoords = get_global_mouse_position()
-		if(slideBeginCoords.x >= xStart and slideBeginCoords.y >= yStart and slideBeginCoords.x < xStart + (width*offset) and slideBeginCoords.y < yStart + (height*offset)):
+		slideBeginPosition = utils.getPositionFromTileCoords(slideBeginCoords, self)
+		var row: int = slideBeginPosition.row
+		var column: int = slideBeginPosition.column
+		if(!emptyTile(row, column) and row >= 0 and column >= 0 and row < 8 and column < 8):
 			slideOngoing = true
 	if Input.is_action_just_released("ui_touch"):
-		slideEndCoords = get_global_mouse_position()
-		if (slideOngoing and slideEndCoords.x >= xStart and slideEndCoords.y >= yStart and slideEndCoords.x < xStart + (width*offset) and slideEndCoords.y < yStart + (height*offset)):
+		var slideEndCoords = get_global_mouse_position()
+		var slideDirection: Vector2 = utils.getSlideDirection(slideBeginCoords, slideEndCoords)
+		slideEndPosition = Position.new(slideBeginPosition.row + slideDirection.y, slideBeginPosition.column + slideDirection.x)
+		if (!emptyTile(slideEndPosition.row, slideEndPosition.column) and slideOngoing):
 			state = treatMove
 		else:
 			state = checkMove
@@ -92,14 +106,10 @@ func checkMovement() -> void:
 func treatMovement() -> void:
 	state = wait
 
-	var slideDirection: Vector2 = utils.getSlideDirection(slideBeginCoords, slideEndCoords)
-	var firstBlockPosition: Position = utils.getPositionFromTileCoords(slideBeginCoords, self)
-	var secondBlockPosition: Position = Position.new(firstBlockPosition.row + slideDirection.y, firstBlockPosition.column + slideDirection.x)
-
-	if not firstBlockPosition.equals(secondBlockPosition):
-		await swapBlocks(firstBlockPosition, secondBlockPosition)
+	if not slideBeginPosition.equals(slideEndPosition):
+		await swapBlocks(slideBeginPosition, slideEndPosition)
 		if not getMatchesOnGrid():
-			await swapBlocks(firstBlockPosition, secondBlockPosition)
+			await swapBlocks(slideBeginPosition, slideEndPosition)
 
 	state = resolveMatches
 
@@ -120,17 +130,18 @@ func getMatchesOnGrid() -> bool:
 	var thereIsAMatch: bool = false
 	for row in height:
 		for column in width:
-			var blockType: String = grid[row][column].blockType
-			if(row >= 2 && grid[row-1][column].blockType == blockType and grid[row-2][column].blockType == blockType):
-				grid[row-2][column].partOfMatch = true
-				grid[row-1][column].partOfMatch = true
-				grid[row][column].partOfMatch = true
-				thereIsAMatch = true
-			if(column >= 2 && grid[row][column-1].blockType == blockType and grid[row][column-2].blockType == blockType):
-				grid[row][column-2].partOfMatch = true
-				grid[row][column-1].partOfMatch = true
-				grid[row][column].partOfMatch = true
-				thereIsAMatch = true
+			if !emptyTile(row,column):
+				var blockType: String = grid[row][column].blockType
+				if(row >= 2 && !emptyTile(row-1,column) && grid[row-1][column].blockType == blockType and !emptyTile(row-2,column) && grid[row-2][column].blockType == blockType):
+					grid[row-2][column].partOfMatch = true
+					grid[row-1][column].partOfMatch = true
+					grid[row][column].partOfMatch = true
+					thereIsAMatch = true
+				if(column >= 2 && !emptyTile(row,column-1) && grid[row][column-1].blockType == blockType and !emptyTile(row,column-2) && grid[row][column-2].blockType == blockType):
+					grid[row][column-2].partOfMatch = true
+					grid[row][column-1].partOfMatch = true
+					grid[row][column].partOfMatch = true
+					thereIsAMatch = true
 	return thereIsAMatch
 
 func resolve() -> void:
@@ -163,16 +174,31 @@ func deleteMatches() -> void:
 		grid[i][j].queue_free()
 		grid[i][j] = null
 
+func getBlocksDown2() -> void:
+	var lastSignal
+	for i in range(height-1,-1,-1):
+		for j in width:
+			if !grid[i][j] and !emptyTile(i,j):
+				for k in range(i-1,-1,-1):
+					if grid[k][j]:
+						lastSignal = grid[k][j].move(utils.getTileCoordsFromPosition(Position.new(i, j), self))
+						grid[i][j] = grid[k][j]
+						grid[k][j] = null
+						break
+	if lastSignal:
+		await lastSignal
+		
 func getBlocksDown() -> void:
 	var lastSignal
 	for i in height:
 		for j in width:
-			if !grid[i][j]:
-				for k in range(i-1,-1,-1):
-					if grid[k][j]:
-						lastSignal = grid[k][j].move(utils.getTileCoordsFromPosition(Position.new(k+1, j), self))
-						grid[k+1][j] = grid[k][j]
-						grid[k][j] = null
+			if !grid[i][j] and !emptyTile(i,j):
+				var k: int = i-1
+				while(k >= 0 and grid[k][j]):
+					lastSignal = grid[k][j].move(utils.getTileCoordsFromPosition(Position.new(k+1, j), self))
+					grid[k+1][j] = grid[k][j]
+					grid[k][j] = null
+					k -= 1
 	if lastSignal:
 		await lastSignal
 	
@@ -180,7 +206,7 @@ func fillEmptyBlocks() -> void:
 	var lastSignal
 	for i in height:
 		for j in width:
-			if grid[i][j] == null:
+			if grid[i][j] == null and !emptyTile(i,j):
 				var block:Node = Block.createRandomBlock()
 				block.position = utils.getTileCoordsFromPosition(Position.new(i,j), self)
 				add_child(block)
