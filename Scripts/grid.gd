862cdb1f17a5376944e8ccb81869446a7db08acc
BlaiseI <BlaiseWorlds@gmail.com>
2025-09-09 10:32:45 +0200

    Revert "Modified Ortie"
    This reverts commit 01c1bf65eb532fefbafdbd0f4f7aae17bdd05c82.

--- Scripts/grid.gd
+++ Scripts/grid.gd
@@ -35,10 +35,11 @@ func createEmptyGrid() -> void:
 		grid.append([])
 		for j in width:
 			grid[i].append(null)
-			var cadre: Block = cadrePreload.instantiate()
-			add_child(cadre)
-			cadre.position = utils.getTileCoordsFromPosition(Position.new(i,j), self)
-			#cadre.get_node("CenterContainer/Control/Sprite2D").modulate.a = 0.2
+			if !emptyTile(i,j):
+				var cadre: Block = cadrePreload.instantiate()
+				add_child(cadre)
+				cadre.position = utils.getTileCoordsFromPosition(Position.new(i,j), self)
+				#cadre.get_node("CenterContainer/Control/Sprite2D").modulate.a = 0.2
 
 func fillGrid() -> void:
 	for i in height:
