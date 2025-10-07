c6481e03624f1173aa171dfb74c24a4d48489ace
BlaiseI <BlaiseWorlds@gmail.com>
2025-10-07 10:32:09 +0200

    Level creator manages nb moves left, conditions types and numbers, and nb different blocks

--- Scripts/LevelCreator.gd
+++ Scripts/LevelCreator.gd
@@ -13,6 +13,13 @@ var webs = []
 var cadrePreload = preload("res://Scenes/cadre.tscn")
 var webPreload = preload("res://Scenes/web.tscn")
 var selectedBlock: String = "chardon"
+var condition1: String = "null"
+var numberForCondition1 = 0
+var condition2: String = "null"
+var numberForCondition2 = 0
+var nbMovesLeft = 0
+var nbDifferentBlocks: int = 4
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -38,13 +45,13 @@ func saveLevel() -> void:
 		"gridRonce" : [],
 		"gridLierre" : [],
 		"fixedBlocks" : [],
-		"nbDifferentBlocks":4,
+		"nbDifferentBlocks":nbDifferentBlocks,
 		"nbCarrots":1,
-		"ConditionType1":"chardon",
-		"numberForCondition1":11,
-		"ConditionType2":"null",
-		"numberForCondition2":0,
-		"numberMovesLeft":3
+		"ConditionType1":condition1,
+		"numberForCondition1":numberForCondition1,
+		"ConditionType2":condition2,
+		"numberForCondition2":numberForCondition2,
+		"numberMovesLeft":nbMovesLeft
 	}
 	for i in height:
 		for j in width:
@@ -87,10 +94,21 @@ func isInGrid(coords: Vector2) -> bool:
 			return false
 	return true
 
+func isInCondition1(coords: Vector2) -> bool:
+	if(coords.x < 370 or coords.x > 430 or coords.y < 860 or coords.y > 920):
+			return false
+	return true
+
+func isInCondition2(coords: Vector2) -> bool:
+	if(coords.x < 480 or coords.x > 540 or coords.y < 860 or coords.y > 920):
+			return false
+	return true
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("ui_touch"):
 		var touchCoords: Vector2 = get_global_mouse_position()
+		print(touchCoords)
 		if isInGrid(touchCoords):
 			var tileTouched: Vector2 = getTilePositionFromCoords(touchCoords)
 			var block
@@ -103,6 +121,34 @@ func _process(delta: float) -> void:
 				remove_child(grid[tileTouched.x][tileTouched.y])
 			grid[tileTouched.x][tileTouched.y] = block
 			add_child(block)
+		elif isInCondition1(touchCoords):
+			print("in condition 1")
+			var block
+			if(selectedBlock == "web"):
+				block = webPreload.instantiate()
+			else:
+				block = Block.createBlock(selectedBlock)
+			block.position = Vector2(400,890)
+			block.name = "condition1"
+			var last_block = find_child("condition1", true, false)
+			if(last_block):
+				remove_child(last_block)
+			add_child(block)
+			condition1 = selectedBlock
+		elif isInCondition2(touchCoords):
+			print("in condition 2")
+			var block
+			if(selectedBlock == "web"):
+				block = webPreload.instantiate()
+			else:
+				block = Block.createBlock(selectedBlock)
+			block.position = Vector2(510, 890)
+			block.name = "condition2"
+			var last_block = find_child("condition2", true, false)
+			if(last_block):
+				remove_child(last_block)
+			add_child(block)
+			condition2 = selectedBlock
 
 func treatInput(type: String) -> void:
 	if type.contains("height"):
@@ -133,3 +179,31 @@ func treatInput(type: String) -> void:
 		if type.contains("sub"):
 			levelName = str(int(levelName)-1)
 			$"LevelSelect/Number".text = levelName
+	if type.contains("moves"):
+		if type.contains("add"):
+			nbMovesLeft += 1
+			$"NbMovesLeftSelect/Number".text = str(nbMovesLeft)
+		if type.contains("sub"):
+			nbMovesLeft -= 1
+			$"NbMovesLeftSelect/Number".text = str(nbMovesLeft)
+	if type.contains("condition1"):
+		if type.contains("add"):
+			numberForCondition1 += 1
+			$"Condition1Select/Number".text = str(numberForCondition1)
+		if type.contains("sub"):
+			numberForCondition1 -= 1
+			$"Condition1Select/Number".text = str(numberForCondition1)
+	if type.contains("condition2"):
+		if type.contains("add"):
+			numberForCondition2 += 1
+			$"Condition2Select/Number".text = str(numberForCondition2)
+		if type.contains("sub"):
+			numberForCondition2 -= 1
+			$"Condition2Select/Number".text = str(numberForCondition2)
+	if type.contains("difBlocks"):
+		if type.contains("add"):
+			nbDifferentBlocks += 1
+			$"DifBlockSelect/Number".text = str(nbDifferentBlocks)
+		if type.contains("sub"):
+			nbDifferentBlocks -= 1
+			$"DifBlockSelect/Number".text = str(nbDifferentBlocks)
